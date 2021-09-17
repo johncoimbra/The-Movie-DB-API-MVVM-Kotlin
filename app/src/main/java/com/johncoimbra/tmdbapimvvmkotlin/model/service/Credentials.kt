bd49031f97bc5444e471ef2e6058faeead0e042c
John Coimbra <johnallen.inf@gmail.com>
2021-09-17 19:47:58 -0300

    Created Constants Object

---
+++ app/src/main/java/com/johncoimbra/tmdbapimvvmkotlin/model/service/Credentials.kt
@@ -0,0 +1,7 @@
+package com.johncoimbra.tmdbapimvvmkotlin.model.service
+
+object Credentials {
+    const val BASE_URL: String = "https://api.themoviedb.org"
+    const val API_KEY: String = "1a038e71eba0790d5d7f8f949ab41f75"
+    const val BASE_URL_IMAGE = "https://image.tmdb.org/t/p/w500"
+}
