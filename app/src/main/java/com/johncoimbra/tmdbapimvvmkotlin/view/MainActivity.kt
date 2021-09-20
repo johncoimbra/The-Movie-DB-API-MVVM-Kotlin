7d87698f7bec218e4bd0d7583b6dbe9b68f48155
John Coimbra <johnallen.inf@gmail.com>
2021-09-20 00:32:10 -0300

    Created MainActivity

--- app/src/main/java/com/johncoimbra/tmdbapimvvmkotlin/view/MainActivity.kt
+++ app/src/main/java/com/johncoimbra/tmdbapimvvmkotlin/view/MainActivity.kt
@@ -4,46 +4,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.johncoimbra.tmdbapimvvmkotlin.R
-import com.johncoimbra.tmdbapimvvmkotlin.databinding.ActivityMoviesBinding
+import com.johncoimbra.tmdbapimvvmkotlin.databinding.ActivityMainBinding
 import com.johncoimbra.tmdbapimvvmkotlin.extensions.addFragment
 
-class MoviesActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMoviesBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMoviesBinding.inflate(layoutInflater)
+        Thread.sleep(2000)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setTheme(R.style.Theme_TMDbAPIMVVMKotlin)
         setContentView(binding.root)
         supportActionBar?.hide()
 
         loadFragment(NowPlayingFragment.newInstance())
-
-        binding.bottomMenu.setOnItemSelectedListener { item ->
-            when(item.itemId){
-                R.id.nav_now_playing -> {
-                    val fragment = NowPlayingFragment.newInstance()
-                    loadFragment(fragment)
-                    return@setOnItemSelectedListener true
-                }
-
-                R.id.nav_search -> {
-                    val fragment = SearchFragment.newInstance()
-                    loadFragment(fragment)
-                    return@setOnItemSelectedListener true
-                }
-
-                R.id.nav_favorite -> {
-                    val fragment = FavoriteFragment.newInstance()
-                    loadFragment(fragment)
-                    return@setOnItemSelectedListener true
-                }
-            }
-            false
-        }
     }
 
-    fun loadFragment(mFragmentLoaded: Fragment){
+    fun loadFragment(mFragmentLoaded: Fragment) {
         addFragment(mFragmentLoaded, supportFragmentManager, binding.content)
     }
 }
