2ad54b3e35ff6fb75943bb256fe171dadc99e221
John Coimbra <johnallen.inf@gmail.com>
2021-09-20 00:27:40 -0300

    Created Model Class MovieModel

--- app/src/main/java/com/johncoimbra/tmdbapimvvmkotlin/model/entity/MovieModel.kt
+++ app/src/main/java/com/johncoimbra/tmdbapimvvmkotlin/model/entity/MovieModel.kt
@@ -3,11 +3,10 @@ package com.johncoimbra.tmdbapimvvmkotlin.model.entity
 import com.google.gson.annotations.SerializedName
 
 data class MovieModel(
-    @SerializedName("id") val movie_id: Long,
+    @SerializedName("id") val movie_id: Int,
     @SerializedName("title") val title: String,
     @SerializedName("overview") val overview: String,
     @SerializedName("poster_path") val posterPath: String,
-    @SerializedName("release_date") val releaseDate: String,
-    @SerializedName("backdrop_path") val backdropPath: String,
-    @SerializedName("vote_average") val rating: Float
+    @SerializedName("popularity") val popularity: Double,
+    @SerializedName("vote_count") val vote_count: Int
 )
