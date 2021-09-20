ad8f9611c02a4951e4d491b3a4f507a663ba380a
John Coimbra <johnallen.inf@gmail.com>
2021-09-20 00:35:45 -0300

    Created ViewModels

---
+++ app/src/main/java/com/johncoimbra/tmdbapimvvmkotlin/viewmodel/NowPlayingViewModel.kt
@@ -0,0 +1,28 @@
+package com.johncoimbra.tmdbapimvvmkotlin.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.johncoimbra.tmdbapimvvmkotlin.model.entity.MovieModel
+import com.johncoimbra.tmdbapimvvmkotlin.model.repository.MovieRepository
+import com.johncoimbra.tmdbapimvvmkotlin.utils.Event
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class NowPlayingViewModel : ViewModel() {
+
+    private val repository = MovieRepository()
+    val nowPlaying = MutableLiveData<List<MovieModel>>()
+    val nowPlayingError = MutableLiveData<Event<Unit>>()
+
+    fun getNowPlaying(page: Int) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val movieResponse = repository.getNowPlaying(page)
+            if (movieResponse == null) {
+                nowPlayingError.postValue(Event(Unit))
+            } else {
+                nowPlaying.postValue(movieResponse.movies)
+            }
+        }
+    }
+}
