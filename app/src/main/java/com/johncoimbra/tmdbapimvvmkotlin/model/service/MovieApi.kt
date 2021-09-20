499744a6913713547023d7416b0ba354269e0bdf
John Coimbra <johnallen.inf@gmail.com>
2021-09-20 00:31:46 -0300

    Created Retrofit Api Class

--- app/src/main/java/com/johncoimbra/tmdbapimvvmkotlin/model/service/MovieApi.kt
+++ app/src/main/java/com/johncoimbra/tmdbapimvvmkotlin/model/service/MovieApi.kt
@@ -1,16 +1,25 @@
 package com.johncoimbra.tmdbapimvvmkotlin.model.service
 
 import com.johncoimbra.tmdbapimvvmkotlin.model.entity.MovieResponse
-import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface MovieApi {
-
     @GET("/3/movie/now_playing")
     suspend fun getNowPlaying(
         @Query("api_key") key: String,
         @Query("page") page: Int
     ): Response<MovieResponse>
+
+    @GET("/3/movie/{movie_id}/similar?")
+    suspend fun getSimilar(
+        @Path("movie_id") movieId: Int,
+        @Query("api_key") key: String,
+        @Query("page") page: Int
+    ): Response<MovieResponse>
 }
+
+
+
